4a7c992c1e7fbe8d2d14f92a7b54fca518470293
Hao Nghiem <qtvhao@gmail.com>
2025-05-16 22:48:20 +0700

    Add

---
+++ src/Infrastructure/Bootstrap/ServiceProvider.ts
@@ -0,0 +1,64 @@
+import { Container } from 'inversify';
+
+export abstract class ServiceProvider {
+  protected app: Container;
+
+  protected static publishes: Record<string, string[]> = {};
+  private bootingCallbacks: (() => void)[] = [];
+  private bootedCallbacks: (() => void)[] = [];
+
+  constructor(appInstance: Container) {
+    this.app = appInstance;
+  }
+
+  /**
+   * Register bindings in the IoC container
+   */
+  public abstract register(): void;
+
+  /**
+   * Register a callback to run during the booting phase
+   */
+  public booting(callback: () => void): void {
+    this.bootingCallbacks.push(callback);
+  }
+
+  /**
+   * Register a callback to run after the service has booted
+   */
+  public booted(callback: () => void): void {
+    this.bootedCallbacks.push(callback);
+  }
+
+  /**
+   * Execute all booting callbacks
+   */
+  public callBootingCallbacks(): void {
+    for (const callback of this.bootingCallbacks) {
+      callback();
+    }
+  }
+
+  /**
+   * Execute all booted callbacks
+   */
+  public callBootedCallbacks(): void {
+    for (const callback of this.bootedCallbacks) {
+      callback();
+    }
+  }
+
+  /**
+   * Declare the services this provider registers
+   */
+  public provides(): string[] {
+    return [];
+  }
+
+  /**
+   * Specify conditions under which this provider should be loaded
+   */
+  public when(): string[] {
+    return [];
+  }
+}
