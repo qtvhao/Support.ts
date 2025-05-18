15e1a3f0898b7325bb390ea751403623ae9a8296
Hao Nghiem <qtvhao@gmail.com>
2025-05-18 16:42:47 +0700

    Add

--- src/Infrastructure/Messaging/EventMapperRegistry.ts
+++ src/Infrastructure/Messaging/EventMapperRegistry.ts
@@ -5,16 +5,17 @@ import { IDomainEvent, IEventMapper, IEventMapperRegistry } from "contracts.ts";
  * Maintains a registry of event mappers by event name.
  * Supports adding and retrieving mappers for serialization and deserialization.
  */
-export type EventCtor<T extends IDomainEvent> = new (...args: any[]) => T;
-
 export class EventMapperRegistry implements IEventMapperRegistry {
-  private readonly mappers: Map<Function, IEventMapper<any, any>> = new Map();
+  private readonly mappers: Map<string, IEventMapper<any, any>> = new Map();
 
-  get<T extends IDomainEvent>(ctor: EventCtor<T>): IEventMapper<any, T> | undefined {
-    return this.mappers.get(ctor) as IEventMapper<any, T> | undefined;
+  get(eventName: string): IEventMapper<any, any> | undefined {
+    return this.mappers.get(eventName);
   }
 
-  set<T extends IDomainEvent, U>(ctor: EventCtor<T>, mapper: IEventMapper<U, T>): void {
-    this.mappers.set(ctor, mapper);
+  set<T extends IDomainEvent, U>(
+    eventName: string,
+    mapper: IEventMapper<U, T>,
+  ): void {
+    this.mappers.set(eventName, mapper);
   }
 }
