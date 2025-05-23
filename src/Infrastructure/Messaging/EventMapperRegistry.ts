36b68d9e0c23e5f6c2ea582e1643d3963cbf253b
Hao Nghiem <qtvhao@gmail.com>
2025-05-23 13:16:48 +0700

    Add

--- src/Infrastructure/Messaging/EventMapperRegistry.ts
+++ src/Infrastructure/Messaging/EventMapperRegistry.ts
@@ -1,20 +1,19 @@
-// Support.ts/src/Infrastructure/Messaging/EventMapperRegistry.ts
-import { IDomainEvent, IEventMapper, IEventMapperRegistry } from "contracts.ts";
+import { IDomainEvent, IDomainEventMapper, IDomainEventMapperRegistry } from "contracts.ts";
 
 /**
  * Maintains a registry of event mappers by event name.
  * Supports adding and retrieving mappers for serialization and deserialization.
  */
-export class EventMapperRegistry implements IEventMapperRegistry {
-  private readonly mappers: Map<string, IEventMapper<any, any>> = new Map();
+export class EventMapperRegistry implements IDomainEventMapperRegistry {
+  private readonly mappers: Map<string, IDomainEventMapper<any, any>> = new Map();
 
-  get(eventName: string): IEventMapper<any, any> | undefined {
+  get(eventName: string): IDomainEventMapper<any, any> | undefined {
     return this.mappers.get(eventName);
   }
 
   set<T extends IDomainEvent, U>(
     eventName: string,
-    mapper: IEventMapper<U, T>,
+    mapper: IDomainEventMapper<U, T>,
   ): void {
     this.mappers.set(eventName, mapper);
   }
