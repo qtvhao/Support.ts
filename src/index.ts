89f7aaf44dd1e8908369d0de7d4dc85621136e27
Hao Nghiem <qtvhao@gmail.com>
2025-05-23 21:12:53 +0700

    Add

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { ServiceProvider } from "./Infrastructure/Bootstrap/ServiceProvider";
 import { DomainEventMapperRegistry } from "./Infrastructure/Messaging/DomainEventMapperRegistry";
-import { EventTopicMapper } from "./Infrastructure/Messaging/Kafka/EventTopicMapper";
 
-export { DomainEventMapperRegistry, ServiceProvider, EventTopicMapper };
+export { DomainEventMapperRegistry, ServiceProvider };
